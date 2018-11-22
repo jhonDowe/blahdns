45260c82dff46eeaaef47e5eb7dab27a4a67c119
ookangzheng <okz5289@gmail.com>
2018-11-22 18:37:38 +0800

    added simple knot-resolver monitor script and added into crontab

---
+++ server-conf/monitor-kresd.sh
@@ -0,0 +1,24 @@
+#if lsof -Pi :50 -sUDP:LISTEN -t >/dev/null ; then
+#    echo "running"
+#else
+#    echo "not running"
+#fi
+
+#netstat -ln | grep ":50 " 2>&1 > /dev/null 
+#if [ $? -eq 1 ]; then   
+#     echo Starting Meteor; 
+#fi
+
+#!/bin/bash
+
+# Check if gedit is running
+# -x flag only match processes whose name (or command line if -f is
+# specified) exactly match the pattern. 
+
+if pgrep -x "kresd" > /dev/null
+then
+    echo "kresd is Running"
+else
+    /root/kresd.sh
+    echo "kresd is Stopped"
+fi
