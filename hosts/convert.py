bb69fef20fc363ab0f927727f60facef22d82df9
ookangzheng <okz5289@gmail.com>
2018-11-28 14:15:02 +0800

    Hosts update

--- hosts/convert.py
+++ hosts/convert.py
@@ -7,10 +7,5 @@ outputFile = "rpz.blacklist"
 with open(inputFile, 'r') as f: # load file
     lines = f.read().splitlines()  # read lines
 
-with open(outputFile, 'w') as f: 
+with open(outputFile, 'w') as f:
     f.write('\n'.join([line + '  CNAME .' for line in lines]))
-
-# stringToAPpend = "CNAME . "
-# with open(inputFile, 'r') as inFile, open(outputFile, 'w') as outFile:
-#     for line in inFile:
-#         outFile.write(stringToAPpend+line)
