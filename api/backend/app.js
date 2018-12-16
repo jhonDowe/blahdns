89c45089e13a7d90025c5588b6f245e406cba381
ookangzheng <okz5289@gmail.com>
2018-12-17 04:21:55 +0800

    add abort into api

--- api/backend/app.js
+++ api/backend/app.js
@@ -1,5 +1,8 @@
 const service = require("restana")();
-const { Resolver } = require("dns").promises;
+const AbortController = require("abort-controller")
+const {
+  Resolver
+} = require("dns").promises;
 const resolver = new Resolver();
 const cors = require("cors");
 service.use(
@@ -12,7 +15,6 @@ service.use(
   })
 );
 
-
 service.get("/", function (req, res) {
   res.send({
     hello: "world"
@@ -22,40 +24,52 @@ service.get("/", function (req, res) {
 resolver.setServers(["159.69.198.101"]);
 
 service.get("/api/:domainName", function (req, res) {
-  let timeout;
+  let controller = new AbortController();
+  let signal = controller.signal;
+  let timeout = 3000;
+
   function myTimer() {
     timeout = setTimeout(() => {
+      controller.abort();
       res.send({
         status: "timeout"
       });
-    }, 3000);
-
+    }, timeout);
   }
-  myTimer()
-  resolver.resolve(`${req.params.domainName}`)
-    .then((addresses) => {
-      clearTimeout(timeout)
+  myTimer();
+  resolver
+    .resolve(`${req.params.domainName}`)
+    .then(addresses => {
+      clearTimeout(timeout);
       res.send({
         status: "passed"
       });
-    }).catch((error) => {
-      if (error.code === 'NOTFOUND' || error.code === 'SERVFAIL' || error.code === 'REFUSED') {
-        clearTimeout(timeout)
+    })
+    .catch(error => {
+      if (
+        error.code === "NOTFOUND" ||
+        error.code === "SERVFAIL" ||
+        error.code === "REFUSED"
+      ) {
+        clearTimeout(timeout);
         res.send({
-          status: 'failed'
-        })
-      } else if (error.code === 'TIMEOUT') {
-        clearTimeout(timeout)
+          status: "failed"
+        });
+      } else if (error.code === "TIMEOUT") {
+        clearTimeout(timeout);
         res.send({
-          status: 'timeout'
-        })
-      } else if (error.code === 'ENOTFOUND' || error.code === 'NXDOMAIN' || error.code === 'NOTIMP') {
-        clearTimeout(timeout)
+          status: "timeout"
+        });
+      } else if (
+        error.code === "ENOTFOUND" ||
+        error.code === "NXDOMAIN" ||
+        error.code === "NOTIMP"
+      ) {
+        clearTimeout(timeout);
         res.send({
-          status: 'blocked'
-        })
+          status: "blocked"
+        });
       }
-    }
-    )
-})
-service.start(3000)
+    });
+});
+service.start(3000);
